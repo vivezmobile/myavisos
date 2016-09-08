4350c5268169af1f8221a5321e2f52415fc47930
rubenmartin <rmartinramos@soldevelo.com>
2016-09-08 20:35:48 +0200

    Commit anadimos los items del menu

--- app/src/main/java/com/example/rmartin/avisos/AvisosActivity.java
+++ app/src/main/java/com/example/rmartin/avisos/AvisosActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,7 @@ import android.widget.ListView;
 public class AvisosActivity extends AppCompatActivity {
 
     private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,16 +40,15 @@ public class AvisosActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_nuevo:
+                Log.d(getLocalClassName(), "Crear Nuevo");
+                return true;
+            case R.id.action_salir:
+                finish();
+                return true;
+            default:
+                return false;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
